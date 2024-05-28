ecb9b8c45e68a63bb6373378c0bdb0adb4a4e913
Harsh Garg <hgarg5492@gmail.com>
2024-05-28 19:06:59 +0530

    Image and Next JS Config Bug Fixed

--- next.config.js
+++ next.config.js
@@ -5,43 +5,6 @@ module.exports = {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'localhost',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '127.0.0.1',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'iharsh3289.github.io',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: 'iharsh3289.github.io',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    domains: ['localhost', '127.0.0.1', 'iharsh3289.netflify.app'],
   },
 }
